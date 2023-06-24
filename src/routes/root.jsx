900ca65c86fb50dbfd39bde2a96e6d204c186154
Sarylov <jalsan.work@yandex.ru>
2023-06-25 02:44:48 +0300

    add padding

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -4,7 +4,7 @@ import { ArticleItem } from "../components/article-item";
 export const Root = () => {
   return (
     <div className="flex justify-center text-white bg-bgColor-100 min-h-screen">
-      <div className="w-container flex items-center mt-10 flex-col">
+      <div className="w-container flex items-center mt-10 flex-col px-2">
         {/* <div className="w-3/4 flex gap-2"> */}
         {/* <Input className="w-full" label="Поиск" /> */}
         {/* <Button className=" bg-purple-800">Искать</Button>
