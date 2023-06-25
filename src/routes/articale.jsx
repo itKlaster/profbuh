225d866b1539a7cac2a4151301fa6472a51dbcaa
Nefelit <saphirepi@vk.com>
2023-06-25 06:50:25 +0300

    ДВУХСТОРОНЯЯ ПОДДЕРЖКА LTS!!! :sparkles:

--- src/routes/articale.jsx
+++ src/routes/articale.jsx
@@ -53,26 +53,6 @@ export const Article = () => {
   }, [article]);
 
   return (
-      <ExpandedImageContext.Provider value={fullImage}>
-        {fullImage.image && <ExpandedImage />}
-      </ExpandedImageContext.Provider>
-      <div className="w-container px-2 flex justify-center items-start mt-10 flex-col">
-        <Link to="/">
-          <Button className="flex items-center gap-2">
-            <ArrowLongLeftIcon className="w-4 h-4" /> Назад
-          </Button>
-        </Link>
-        <Typography className="mb-8 text-4xl text-left font-bold text-textColor-100 mt-10">
-          {article?.data.title}
-        </Typography>
-        <Typography className="">{article?.data.description}</Typography>
-        <div className="mt-5">
-          <span className="text-textColor-100 text-2xl"> Содержание:</span>
-          {article?.data.topics.map((topic, index) => (
-            <li key={index} className="mt-2">
-              <a className="underline hover:text-main" href={"#" + index}>
-                {topic.title}
-              </a>
     <>
       <header class="container mx-auto py-10 flex justify-between">
         <div class="logo text-4xl font-black">БухЭксперт8</div>
@@ -81,44 +61,53 @@ export const Article = () => {
             <li>
               <button class="btn btn-primary px-10 py-2 rounded-lg text-white">Главная</button>
             </li>
-          ))}
-        </div>
-        <div className="w-full flex flex-col gap-20 mb-20 mt-14">
-          {topics?.map((topic, index) => (
-            <div key={index} className="" id={index}>
-              <Typography className="font-normal mb-4 text-2xl text-left text-textColor-100 mt-5">
-                {topic.title}
-              </Typography>
-              <div className="flex flex-col gap-5 ">
-                <div className="flex items-center gap-2">
-                  {topic.images.map((image, index) => (
-                    <div
-                      key={index}
-                      className="m-0 rounded-xl flex-grow"
-                      onClick={() =>
-                        fullImage.showImage("data:image/png;base64," + image)
-                      }
-                    >
-                      <img
-                        className="w-full h-full object-cover rounded-md cursor-pointer"
-                        src={"data:image/png;base64," + image}
-                        alt=""
-                      />
-                    </div>
-                  ))}
-                </div>
-                <div className="font-normal mb-8 flex flex-col ">
-                  {topic.paragraphs.map(({ time, text }, index) => (
-                    <div key={index} className="flex gap-5 mt-2">
-                      <a
-                        href="#"
-                        className="p-1 px-2 hover:bg-main bg-bgColor-200 text-textColor-100 self-start rounded-md"
-                      >
-                        {time}
-                      </a>
-                      <p>{text}</p>
-                    </div>
-                  ))}
+          </ul>
+        </nav>
+      </header>
+      <div className="container mx-auto text-textColor-200">
+        <ExpandedImageContext.Provider value={fullImage}>
+          {fullImage.image && <ExpandedImage />}
+        </ExpandedImageContext.Provider>
+        <div className="w-container px-2 flex justify-center items-start mt-10 flex-col">
+          <Link to="/">
+            <Button className="flex items-center gap-2">
+              <ArrowLongLeftIcon className="w-4 h-4" /> Назад
+            </Button>
+          </Link>
+          <Typography className="mb-8 text-4xl text-left font-bold text-textColor-100 mt-10">
+            {article?.data?.data?.title}
+          </Typography>
+          <Typography className="">{article?.data?.description}</Typography>
+          <div className="mt-5">
+            <span className="text-textColor-100 text-2xl"> Содержание:</span>
+            {article?.data?.data?.topics?.map((topic, index) => (
+              <li key={index} className="mt-2">
+                <a className="underline hover:text-main" href={"#" + index}>
+                  {topic.title}
+                </a>
+              </li>
+            ))}
+          </div>
+          <div className="w-full flex flex-col gap-20 mb-20 mt-14">
+            {topics?.map((topic, index) => (
+              <div key={index} className="" id={index}>
+                <Typography className="font-normal mb-4 text-2xl text-left text-textColor-100 mt-5">
+                  {topic.title}
+                </Typography>
+                <div className="flex flex-col gap-5 ">
+                 
+                  <div className="font-normal mb-8 flex flex-col ">
+                    {topic.paragraphs.map(({ from, to, fallback, text }, index) => (
+                      <div key={index} className="flex gap-5 mt-2">
+                        {from ? <a
+                          href="#"
+                          className="p-1 px-2 hover:bg-main bg-bgColor-200 text-textColor-100 self-start rounded-md"
+                        >
+                          {from}
+                        </a> : null}
+                        <p className={from ? 'ml-0' : 'ml-[103px]'}>{text}</p>
+                      </div>
+                    ))}
 
                   <span className="bg-blue-400 self-start rounded px-4 py-2 mt-5 cursor-pointer text-textColor-100">
                     {topic.start} - {topic.end}
